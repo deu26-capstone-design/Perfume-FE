e05809dad89eac93e78993280e3b3dbaf3817ec0
imyourmxxn <pkm5522@naver.com>
2026-04-23 17:56:05 +0900

    chore: path alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'node:url';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@app': fileURLToPath(new URL('./src/app', import.meta.url)),
-      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
-      '@features': fileURLToPath(new URL('./src/features', import.meta.url)),
-      '@shared': fileURLToPath(new URL('./src/shared', import.meta.url)),
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
 });
