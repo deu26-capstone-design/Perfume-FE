105649508f56dece29ed52a8ff01673db5f1a78a
imyourmxxn <pkm5522@naver.com>
2026-04-25 00:01:49 +0900

    ✨ feat: header.tsx 파일 생성 및 JSX 구조 생성

---
+++ src/widgets/Header/ui/Header.tsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>Header</h1>
+
+      <nav>
+        <a>향수</a>
+        <a>향계열</a>
+        <a>레이어링</a>
+        <a>서비스</a>
+      </nav>
+
+      <button>로그인</button>
+    </header>
+  );
+}
