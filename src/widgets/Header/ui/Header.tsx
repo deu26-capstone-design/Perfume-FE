5ae2de94a4ad4d3586c98561c3afb9c62aa65f10
nxxmruoymi <pkm5522@naver.com>
2026-04-26 21:24:13 +0900

    🎨 style: 코드 상 로그인 상태 변경

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -6,7 +6,7 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
-  const isLogin = true;
+  const isLogin = false;
   const [isOpen, setIsOpen] = useState(false);
 
   return (
