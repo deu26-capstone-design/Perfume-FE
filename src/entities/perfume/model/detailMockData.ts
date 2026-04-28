ea7f73648fc140cbfc58fbdbde92a40fef3ee28a
nxxmruoymi <pkm5522@naver.com>
2026-04-29 02:59:43 +0900

    🎨 style: 임시 향수 데이터 수정

--- src/entities/perfume/model/detailMockData.ts
+++ src/entities/perfume/model/detailMockData.ts
@@ -6,7 +6,8 @@ export const mockPerfumeDetail: PerfumeDetail = {
   brand: 'Lush',
   gender: 'unisex',
   imageUrl: 'https://fimgs.net/mdimg/perfume-thumbs/375x500.10806.jpg',
-  description: '재스민의 짙고 어두운 꽃향기가 처음부터 강하게 존재감을 드러낸다...',
+  description:
+    '재스민의 짙고 어두운 꽃향기가 처음부터 강하게 존재감을 드러낸다. 청순한 꽃향수와 달리 관능적인 면을 가진 재스민이 향의 중심이다. 이 향수가 욕망이라는 이름을 갖는 이유가 첫 향에서부터 명확하다.이 완성된다. 흰 꽃 계열의 특성이 강하게 드러나지만 우디한 깊이가 받쳐주며 단순한 꽃향수로 분류되기 어렵다.향으로는 바닐라의 따뜻한 달콤함과 백단향의 크리미한 나무향이 자리를 잡고, 장미가 얹히며 부드럽게 마무리된다.',
   notes: {
     top: ['재스민'],
     mid: ['일랑일랑'],
