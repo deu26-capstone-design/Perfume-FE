41eb4bcd1fa7d6aa1e7b8a8d88649dd81820bc3c
nxxmruoymi <pkm5522@naver.com>
2026-05-01 15:40:24 +0900

    🎨 style: app.tsx 충돌 방지 주석처리

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,32 +1,14 @@
 // import Header from '@widgets/Header/ui/Header';
+// import PerfumeInfo from '@widgets/PerfumeInfo/ui/PerfumeInfo';
+// import { mockPerfumeDetail } from '@entities/perfume/model/detailMockData';
 
 function App() {
-  return <>
-    {/*<Header />*/}
-  </>;
+  return (
+    <>
+      {/* <Header />
+      <PerfumeInfo perfume={mockPerfumeDetail} /> */}
+    </>
+  );
 }
 
 export default App;
-
-// import { useState, useEffect } from 'react';
-// import { PerfumeCard } from '../entities/perfume/ui/card/PerfumeCard';
-// import { mockPerfumes } from '../entities/perfume/model/mockData';
-// import type { Perfume } from '../entities/perfume/model/types';
-
-// export default function App() {
-//   const [perfume, setPerfume] = useState<Perfume | null>(null);
-
-//   useEffect(() => {
-//     // API 연동 전까지 mock 데이터 사용. 연동 시 fetch 코드로 변경.
-//     const fetchPerfume = () => {
-//       const response = mockPerfumes[1]; //임시 데이터
-//       setPerfume(response);
-//     };
-
-//     fetchPerfume();
-//   }, []);
-
-//   if (!perfume) return null;
-
-//   return <PerfumeCard perfume={perfume} />;
-// }
