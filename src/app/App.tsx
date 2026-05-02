ddc295f4c44887b20dc9d78aef7b82e720298d1f
nxxmruoymi <pkm5522@naver.com>
2026-05-02 16:07:19 +0900

    ✨ feat: 리뷰 요약 app.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,15 @@
 // import Header from '@widgets/Header/ui/Header';
 // import PerfumeInfo from '@widgets/PerfumeInfo/ui/PerfumeInfo';
 // import { mockPerfumeDetail } from '@entities/perfume/model/detailMockData';
-
+// import MainPage from '@pages/main/MainPage';
+import PerfumeReview from '@widgets/PerfumeReview/ui/PerfumeReview';
 function App() {
   return (
     <>
       {/* <Header />
       <PerfumeInfo perfume={mockPerfumeDetail} /> */}
+      {/* <MainPage /> */}
+      <PerfumeReview />
     </>
   );
 }
