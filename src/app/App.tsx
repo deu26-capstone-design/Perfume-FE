b16ede03919ec732810f36bb7bde474c10b50583
sol <card322@naver.com>
2026-05-05 17:21:12 +0900

    chore: 주석 처리

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import Header from '@widgets/Header/ui/Header';
 // import PerfumeDetailPage from '@pages/perfume-detail/ui/PerfumeDetailPage';
 // import MainPage from '@pages/main/MainPage';
+// import LayeringPage from '@pages/Layering/LayeringPage';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       {/* <PerfumeDetailPage /> */}
       {/* <MainPage /> */}
+      {/* <LayeringPage /> */}
     </>
   );
 }
