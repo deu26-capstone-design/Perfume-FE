9481a3ad59e63f4bd4695f57a5732d54e25e61bf
sol <card322@naver.com>
2026-05-05 19:38:36 +0900

    ♻️ refactor: 버튼 type 명시로 submit 동작 방지

--- src/features/layering-mix/ui/LayeringButton.tsx
+++ src/features/layering-mix/ui/LayeringButton.tsx
@@ -8,7 +8,7 @@ interface LayeringButtonProps {
 const LayeringButton: React.FC<LayeringButtonProps> = ({ onClick, label = '조합하기' }) => {
   return (
     <div className="button-container">
-      <button className="layering-button" onClick={onClick}>
+      <button type="button" className="layering-button" onClick={onClick}>
         <span className="button-text">{label}</span>
       </button>
     </div>
