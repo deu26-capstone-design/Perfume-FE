bf110e3663aed2825a291f91d87e4857748c1d21
nxxmruoymi <pkm5522@naver.com>
2026-05-06 21:06:02 +0900

    ♻️ refactor: app.tsx 라우팅 주석 해제 및 복구

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,18 @@
-// import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from '@widgets/Header/ui/Header';
-// import MainPage from '@pages/main/MainPage';
-// import PerfumeDetailPage from '@pages/perfume-detail/ui/PerfumeDetailPage';
-// import LayeringPage from '@pages/Layering/LayeringPage';
+import MainPage from '@pages/main/MainPage';
+import PerfumeDetailPage from '@pages/perfume-detail/ui/PerfumeDetailPage';
+import LayeringPage from '@pages/Layering/LayeringPage';
 
 export default function App() {
   return (
     <>
       <Header />
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/perfume/:id" element={<PerfumeDetailPage />} />
         <Route path="/layering" element={<LayeringPage />} />
-      </Routes> */}
+      </Routes>
     </>
   );
 }
