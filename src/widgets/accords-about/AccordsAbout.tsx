f23477438a562ba15b38fec4c67018d742964f0b
sol <card322@naver.com>
2026-05-10 00:04:37 +0900

    ✨ feat: 향 계열 설명 컴포넌트 구현

---
+++ src/widgets/accords-about/AccordsAbout.tsx
@@ -0,0 +1,22 @@
+import type { Accord } from '@entities/accords/model/accordsType';
+import './AccordsAbout.css';
+
+type AccordsAboutProps = Pick<Accord, 'name' | 'description' | 'imageUrl'>;
+
+const AccordsAbout = ({ name, description, imageUrl }: AccordsAboutProps) => {
+  return (
+    <section className="accords-about">
+      <div className="image-wrapper">
+        <img src={imageUrl} alt={name} className="image" />
+        <div className="title-bar--mobile">{name}</div>
+      </div>
+
+      <div className="text-container">
+        <h2 className="title">{name}</h2>
+        <p className="content">{description}</p>
+      </div>
+    </section>
+  );
+};
+
+export default AccordsAbout;
