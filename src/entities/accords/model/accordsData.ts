08de96079f454e747f6b38cf1d240ca58bd2b404
sol <card322@naver.com>
2026-05-09 23:28:40 +0900

    ✨ feat: 향 계열 데이터 타입 명시 및 임시 데이터 추가

---
+++ src/entities/accords/model/accordsData.ts
@@ -0,0 +1,89 @@
+import type { Accord } from './accordsType';
+
+export const mockAccords: Accord[] = [
+  {
+    name: 'Floral',
+    description: '풍성한 꽃다발을 연상시키는 화사하고 우아한 향기입니다.',
+    imageUrl: 'https://i.pinimg.com/736x/94/15/60/941560cb1eff5571e0f9c76dfa470d69.jpg',
+    notes: [],
+    perfumes: [],
+  },
+  {
+    name: 'Woody',
+    description: '깊은 숲속의 나무와 흙의 내음을 담은 차분하고 묵직한 향기입니다.',
+    imageUrl: 'https://i.pinimg.com/736x/c2/3c/61/c23c61646b04f2a6003ffb2275caf6b0.jpg',
+    notes: [],
+    perfumes: [],
+  },
+  {
+    name: 'Fresh',
+    description: '갓 세탁한 침구처럼 깨끗하고 산뜻한 기분을 선사하는 향기입니다.',
+    imageUrl: 'https://i.pinimg.com/736x/a9/04/bf/a904bf6c29223998d34f4203ebbdb4ee.jpg',
+    notes: [],
+    perfumes: [],
+  },
+  {
+    name: 'Spicy',
+    description: '시나몬이나 후추처럼 톡 쏘는 강렬함과 따뜻한 깊이감이 공존하는 향기입니다.',
+    imageUrl: 'https://i.pinimg.com/1200x/96/b8/f8/96b8f83dff6bb0270e7777a693c03803.jpg',
+    notes: [],
+    perfumes: [],
+  },
+  {
+    name: 'Sweet',
+    description: '사탕이나 초콜릿처럼 달콤하고 사랑스러운 분위기를 연출하는 향기입니다.',
+    imageUrl: 'https://i.pinimg.com/1200x/a0/13/75/a013752a214e75cea3a2351b4dbf0070.jpg',
+    notes: [],
+    perfumes: [],
+  },
+  {
+    name: 'Gourmand',
+    description: '바닐라, 꿀 등 맛있는 음식의 향기를 담아 포근하고 달콤한 계열입니다.',
+    imageUrl: 'https://i.pinimg.com/1200x/59/f5/48/59f5485bd1248bd08ffecbc377144f21.jpg',
+    notes: [],
+    perfumes: [],
+  },
+  {
+    name: 'Musky',
+    description: '살결의 온기처럼 부드럽고 관능적인 매력을 가진 잔향이 매력적인 향기입니다.',
+    imageUrl: 'https://cdn.pixabay.com/photo/2023/06/17/11/31/rose-8069778_960_720.jpg',
+    notes: [],
+    perfumes: [],
+  },
+  {
+    name: 'Green',
+    description: '갓 벤 풀잎이나 잎사귀처럼 싱그럽고 생동감 넘치는 자연의 향기입니다.',
+    imageUrl: 'https://cdn.pixabay.com/photo/2015/07/02/20/14/leaves-829513_1280.jpg',
+    notes: [],
+    perfumes: [],
+  },
+  {
+    name: 'Citrus',
+    description: '레몬, 자몽처럼 상큼하고 톡톡 튀는 에너지를 주는 향기입니다.',
+    imageUrl:
+      'https://images.unsplash.com/photo-1629923464290-008980c6c083?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    notes: [],
+    perfumes: [],
+  },
+  {
+    name: 'Fruity',
+    description: '달콤한 과일의 과즙이 느껴지는 경쾌하고 발랄한 향기입니다.',
+    imageUrl: 'https://img.magnific.com/free-photo/colorful-fruit_1417-1529.jpg',
+    notes: [],
+    perfumes: [],
+  },
+  {
+    name: 'Aromatic',
+    description: '라벤더, 로즈마리 등 허브의 청량함과 편안함을 담은 향기입니다.',
+    imageUrl: 'https://img.magnific.com/free-photo/blue-blooms-coniferous-twigs_23-2148072200.jpg',
+    notes: [],
+    perfumes: [],
+  },
+  {
+    name: 'Earthy/Smoky',
+    description: '마른 장작이 타는 연기나 비 온 뒤 젖은 흙처럼 깊고 독특한 분위기의 향기입니다.',
+    imageUrl: 'https://i.pinimg.com/736x/18/9c/58/189c5830da466b73f0dcb6476fdaab6f.jpg',
+    notes: [],
+    perfumes: [],
+  },
+];
